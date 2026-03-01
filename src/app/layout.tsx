728c84cc1e4cc50cf615f2d0a024594aac82eea0
Antonin GENELOT <jaimelesfraisesbleues@gmail.com>
2026-03-01 15:01:17 +0100

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "agnlt64.xyz",
   description: "absolute coding",
+  icons: {
+    icon: '/favicon.svg'
+  }
 };
 
 export default function RootLayout({
