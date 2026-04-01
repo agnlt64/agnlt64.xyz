f36e5665b3990b6f26dc93d608dace50f3a5e9c0
Antonin GENELOT <jaimelesfraisesbleues@gmail.com>
2026-04-01 15:11:09 +0200

    close button zindex

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -36,7 +36,7 @@ export function Header() {
     <>
       <header
         className={cn(`
-          fixed top-0 left-0 right-0 m-2 p-6 z-50
+          fixed top-0 left-0 right-0 m-2 p-6 z-[60]
           transition-all duration-500
           rounded-2xl
           border
